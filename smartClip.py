05f9ab3de6c4574cb2ed74f7304713977dbed87f
Frantisek Siegl <frantisek.siegl@idiada.cz>
2017-06-07 10:53:24 +0200

    Help added.

--- smartClip.py
+++ smartClip.py
@@ -13,7 +13,7 @@ class SmartClipException(Exception): pass
 # ==============================================================================
 
 class SmartClip(object):
-	
+		
 	def __init__(self):
 		
 		try:
@@ -526,10 +526,28 @@ def getEntityProperty(entity, propertyName):
 @ansa.session.defbutton('Mesh', 'SmartClip')
 def smartClip():
 	
+	'''"SmartClip" tool is an utility to make the clip definition as easy as possible.
+	
+According to the guiding CON that must be selected by a user, searches for normals of neighbour faces and defines a base
+coordinate system orientation. Projects points to its boundaries in the correspondent vectors and searches for their projection to
+the clip contra part. Measures projection distance and defines CONNECTOR STOP property.
+In the second step, the user is requested to select nodes for the beam definition connecting the connector element with the clip
+itself and the contra part.
+
+Usage:
+	1. select guiding CON
+	2. select nodes connecting the connector element and the clip contra part
+	3. select nodes connecting the connector element and the clip itself
+
+NOTE:
+	SmartClip requires geometry and FEM model loaded into the one session.
+	Keep FEM model visible (visib switch on) in the time of guiding CON selection for the best result.
+'''
+	
 	smartClip = SmartClip()
 	
 # ==============================================================================
 
-if __name__ == '__main__':
-	smartClip()
+#if __name__ == '__main__':
+#	smartClip()
 
